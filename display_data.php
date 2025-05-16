362b9b3f81cd76b208b3201ec05ad1e4a3aa0b47
enzo-quirici <enzo.pro@ik.me>
2025-05-16 14:03:20 +0200

    Icon Updates

--- display_data.php
+++ display_data.php
@@ -10,16 +10,20 @@ $sql = "SELECT os, Version, cpu, Cores, Threads, gpu, Vram, ram FROM system_spec
 $result = $conn->query($sql);
 
 function getOsIconPath($osName, $osVersion) {
-    if (strpos(strtolower($osName), 'win') !== false) {
-        if (strpos($osName, '7') !== false) return "icon/Windows 7 128x128.png";
-        if (strpos($osName, '8') !== false) return "icon/Windows 8 128x128.png";
-        if (strpos($osName, '8.1') !== false) return "icon/Windows 8.1 128x128.png";
-        if (strpos($osName, '10') !== false) return "icon/Windows 10 128x128.png";
-        if (strpos($osName, '11') !== false) return "icon/Windows 11 128x128.png";
+    $osNameLower = strtolower($osName);
+
+    if (strpos($osNameLower, 'win') !== false) {
+        if (strpos($osNameLower, 'xp') !== false) return "icon/Windows xp 128x128.png";
+        if (strpos($osNameLower, 'vista') !== false) return "icon/Windows vista 128x128.png";
+        if (strpos($osNameLower, '8.1') !== false) return "icon/Windows 8.1 128x128.png";
+        if (strpos($osNameLower, '8') !== false) return "icon/Windows 8 128x128.png";
+        if (strpos($osNameLower, '11') !== false) return "icon/Windows 11 128x128.png";
+        if (strpos($osNameLower, '10') !== false) return "icon/Windows 10 128x128.png";
+        if (strpos($osNameLower, '7') !== false) return "icon/Windows 7 128x128.png";
         return "icon/Microsoft Windows 128x128.png";
-    } elseif (strpos(strtolower($osName), 'mac') !== false) {
+    } elseif (strpos($osNameLower, 'mac') !== false) {
         return getMacOsIconPath($osVersion);
-    } elseif (strpos(strtolower($osName), 'nux') !== false) {
+    } elseif (strpos($osNameLower, 'nux') !== false) {
         return getLinuxOsIconPath($osVersion);
     } else {
         return "icon/Unknown 128x128.png";
@@ -28,24 +32,35 @@ function getOsIconPath($osName, $osVersion) {
 
 function getMacOsIconPath($osVersion) {
     $icons = [
-        "10.9" => "icon/Mac OS 10.9 128x128.png",
-        "10.10" => "icon/Mac OS 10.10 128x128.png",
-        "10.11" => "icon/Mac OS 10.11 128x128.png",
-        "10.12" => "icon/Mac OS 10.12 128x128.png",
-        "10.13" => "icon/Mac OS 10.13 128x128.png",
-        "10.14" => "icon/Mac OS 10.14 128x128.png",
         "10.15" => "icon/Mac OS 10.15 128x128.png",
-        "11" => "icon/Mac OS 11 128x128.png",
-        "12" => "icon/Mac OS 12 128x128.png",
-        "13" => "icon/Mac OS 13 128x128.png",
-        "14" => "icon/Mac OS 14 128x128.png",
-        "15" => "icon/Mac OS 15 128x128.png"
+        "10.14" => "icon/Mac OS 10.14 128x128.png",
+        "10.13" => "icon/Mac OS 10.13 128x128.png",
+        "10.12" => "icon/Mac OS 10.12 128x128.png",
+        "10.11" => "icon/Mac OS 10.11 128x128.png",
+        "10.10" => "icon/Mac OS 10.10 128x128.png",
+        "10.9" => "icon/Mac OS 10.9 128x128.png",
+        "10.8" => "icon/Mac OS 10.8 128x128.png",
+        "10.7" => "icon/Mac OS 10.7 128x128.png",
+        "10.6" => "icon/Mac OS 10.6 128x128.png",
+        "10.5" => "icon/Mac OS 10.5 128x128.png",
+        "10.4" => "icon/Mac OS 10.4 128x128.png",
+        "10.3" => "icon/Mac OS 10.3 128x128.png",
+        "10.2" => "icon/Mac OS 10.2 128x128.png",
+        "10.1" => "icon/Mac OS 10.1 128x128.png",
+        "10.0" => "icon/Mac OS 10.0 128x128.png",
+        "15"    => "icon/Mac OS 15 128x128.png",
+        "14"    => "icon/Mac OS 14 128x128.png",
+        "13"    => "icon/Mac OS 13 128x128.png",
+        "12"    => "icon/Mac OS 12 128x128.png",
+        "11"    => "icon/Mac OS 11 128x128.png"
     ];
+
     foreach ($icons as $key => $iconPath) {
         if (strpos($osVersion, $key) !== false) {
             return $iconPath;
         }
     }
+
     return "icon/Apple Mac OS 128x128.png";
 }
 
@@ -84,16 +99,7 @@ function getCpuIcon($cpuInfo) {
     $cpuInfo = strtolower($cpuInfo);
 
     if (strpos($cpuInfo, 'intel') !== false) {
-        if (preg_match('/(\d+)th gen/', $cpuInfo, $matches)) {
-            $generation = (int)$matches[1];
-            if ($generation < 12) {
-                return "icon/Intel 11 Below 128x128.png";
-            } else {
-                return "icon/Intel 12 After 128x128.png";
-            }
-        } else {
-            return "icon/Intel Legacy 128x128.png";
-        }    
+        return "icon/Intel CPU 128x128.png";   
     } elseif (strpos($cpuInfo, 'ryzen') !== false) {
         return "icon/AMD Ryzen 128x128.png";
     } elseif (strpos($cpuInfo, 'amd') !== false) {
@@ -125,7 +131,7 @@ function getGpuIcon($gpuInfo) {
         if (strpos($gpuInfo, 'arc') !== false) {
             return "icon/Intel ARC 128x128.png";
         } else {
-            return "icon/Intel HD Graphics 128x128.png";
+            return "icon/Intel GPU 128x128.png";
         }
     } elseif (strpos($gpuInfo, 'apple') !== false) {
         return "icon/Apple GPU 128x128.png";
